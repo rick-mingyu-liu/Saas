34d22c5ca9530b1bb8129e521e7528f1210921b7
rick-mingyu-liu <rickmyliu@gmail.com>
2024-09-18 10:22:06 +0800

    create new app "visits" and solve the red error when running the server

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -30,12 +30,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #django-apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # my-apps
+    "visits",
 ]
 
 MIDDLEWARE = [
