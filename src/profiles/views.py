57ba9e7efee7ddb76672ce9fb34cd1d18c37af44
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-07 17:26:21 +0800

    user permission and has_Perm

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -10,7 +10,13 @@ User = get_user_model()
 @login_required
 def profile_view(request, username=None, *args, **kwargs):
     user = request.user
+    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
+    print('user.has_perm("visits.view_pagevisit")', user.has_perm("visits.view_pagevisit"))
     #profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user  
+    if is_me:
+        if user.has_perm("visits.view_pagevisit"):
+            # qs = PageVisit.objects.all()
+            pass
     return HttpResponse(f"Hello there! {username} - {profile_user_obj.id} - {user.id} - {is_me}") #f string
