62befa9b07c3ffc4cfc8407886614fc932677eba
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-09 17:05:01 +0800

    user permission

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<username>/", views.profile_view),
+    path('', views.profile_list_view),
+    path("<username>/", views.profile_detail_view),
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -6,17 +6,24 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+@login_required
+def profile_list_view(request, *args, **kwargs):
+    context = {
+        "object_list": User.objects.filter(is_active=True)
+    }
+    return render(request, "profiles/list.html", context)
+
+
 # Create your views here.
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
+def profile_detail_view(request, username=None, *args, **kwargs):
     user = request.user
-    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
-    print('user.has_perm("visits.view_pagevisit")', user.has_perm("visits.view_pagevisit"))
-    #profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user  
-    if is_me:
-        if user.has_perm("visits.view_pagevisit"):
-            # qs = PageVisit.objects.all()
-            pass
-    return HttpResponse(f"Hello there! {username} - {profile_user_obj.id} - {user.id} - {is_me}") #f string
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
+    }
+    # return HttpResponse(f"Hello there! {username} - {profile_user_obj.id} - {user.id} - {is_me}") #f string
+    return render(request, "profiles/detail.html", context)
