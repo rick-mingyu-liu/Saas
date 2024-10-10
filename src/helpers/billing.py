18d2eac75db52271d15156176357828bde8c08b6
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-10 14:44:27 +0800

    start with stripe

---
+++ src/helpers/billing.py
@@ -0,0 +1,19 @@
+# Set your secret key. Remember to switch to your live secret key in production.
+# See your keys here: https://dashboard.stripe.com/apikeys
+import stripe
+from decouple import config
+
+DJANGO_DEBUG=config('DJANGO_DEBUG',default=False, cast=bool)
+STRIPE_SECRET_KEY=config("SREIPE_SECRET_KEY", default="", cast=str)
+
+if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG:
+    raise ValueError("Invalid stripe key for prod")
+
+
+stripe.api_key = STRIPE_SECRET_KEY
+
+def create_customer():
+    stripe.Customer.create(
+    name="Jenny Rosen",
+    email="jennyrosen@example.com",
+    )
