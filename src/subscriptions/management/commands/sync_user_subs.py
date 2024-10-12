b2da983379ff076b6654e5498e2ed3ad5249b181
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-12 17:41:06 +0800

    filtering models with datetime objects

--- src/subscriptions/management/commands/sync_user_subs.py
+++ src/subscriptions/management/commands/sync_user_subs.py
@@ -5,17 +5,32 @@ import helpers.billing
 class Command(BaseCommand):
     
     def add_arguments(self, parser):
+        parser.add_argument("--day-start", default=0, type=int)
+        parser.add_argument("--day-end", default=0, type=int)
+        parser.add_argument("--days-left", default=0, type=int)
+        parser.add_argument("--days-ago", default=0, type=int)
         parser.add_argument("--clear-dangling", action="store_true", default=False)
+        
     
     def handle(self, *args: Any, **options: Any):
         # python manage.py sync_users_subs --clear-dangling
+        days_left = options.get("days_left") 
+        days_ago = options.get("days_ago")
+        day_start = options.get("day_start")
+        day_end = options.get("day_end")
         clear_dangling = options.get("clear_dangling")
         if clear_dangling:
             print("clear dangling not in use active subs in stripe")
             sub_utils.clear_dangling_subs()
         else:
             print("sync active subs")
-            done = sub_utils.refresh_active_users_subscriptions(active_only=True)
+            done = sub_utils.refresh_active_users_subscriptions(
+                active_only=True,
+                days_left=days_left,
+                day_end=day_end,
+                day_start=day_start,
+                days_ago=days_ago
+            )
             if done:
                 print("Done")
             
--- src/subscriptions/utils.py
+++ src/subscriptions/utils.py
@@ -3,12 +3,25 @@ import helpers.billing
 from django.db.models import Q
 from customers.models import Customer
 
-def refresh_active_users_subscriptions(user_ids=None, active_only=True):
+def refresh_active_users_subscriptions(user_ids=None, 
+                                       active_only=True, 
+                                       days_ago=0,
+                                       days_left=0,
+                                       day_start=0,
+                                       day_end=0
+                                       ):
     qs = UserSubscription.objects.all()
     if active_only:
         qs = qs.by_active_trialing()
     if user_ids is not None:
         qs = qs.by_user_ids(user_ids=user_ids)
+    if days_ago > 0:
+        qs = qs.by_days_ago(days_ago=days_ago)
+    if days_left > 0:
+        qs = qs.by_days_left(days_ago=days_ago)
+    if day_start > 0 and day_end > 0:
+        qs = qs.by_range(day_start=day_start, days_end=day_end)
+        
     complete_count = 0
     qs_count = qs.count()
     active_qs = qs.filter(status=SubscriptionStatus.ACTIVE)
