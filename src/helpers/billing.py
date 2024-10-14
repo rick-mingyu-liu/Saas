9340fadf1357d0c4d9dfac6337930c817e8e4d66
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-14 10:14:29 +0800

    corrections for some functionalities for helpers

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -1,24 +1,22 @@
-# Set your secret key. Remember to switch to your live secret key in production.
-# See your keys here: https://dashboard.stripe.com/apikeys
 import stripe
 from decouple import config
 
 from . import date_utils
 
-DJANGO_DEBUG=config('DJANGO_DEBUG',default=False, cast=bool)
-STRIPE_SECRET_KEY=config("SREIPE_SECRET_KEY", default="", cast=str)
+DJANGO_DEBUG=config("DJANGO_DEBUG", default=False, cast=bool)
+STRIPE_SECRET_KEY=config("STRIPE_SECRET_KEY", default="", cast=str)
 STRIPE_TEST_OVERRIDE = config("STRIPE_TEST_OVERRIDE", default=False, cast=bool)
+
 if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG and not STRIPE_TEST_OVERRIDE:
     raise ValueError("Invalid stripe key for prod")
 
-
 stripe.api_key = STRIPE_SECRET_KEY
 
+
 def serialize_subscription_data(subscription_response):
-    sub_plan = subscription_response.sub_r.plan
-    status = subscription_response.sub_r.status
-    current_period_start = date_utils.timestamp_as_datetime(subscription_response.sub_r.current_period_start)
-    current_period_end = date_utils.timestamp_as_datetime(subscription_response.sub_r.current_period_end)
+    status = subscription_response.status
+    current_period_start = date_utils.timestamp_as_datetime(subscription_response.current_period_start)
+    current_period_end = date_utils.timestamp_as_datetime(subscription_response.current_period_end)
     cancel_at_period_end = subscription_response.cancel_at_period_end
     return {
         "current_period_start": current_period_start,
@@ -27,60 +25,63 @@ def serialize_subscription_data(subscription_response):
         "cancel_at_period_end": cancel_at_period_end,
     }
 
-def create_customer(name="", 
-                    email="", 
-                    metadata={},
-                    raw=False):
+def create_customer(
+        name="", 
+        email="", 
+        metadata={},
+        raw=False):
     response = stripe.Customer.create(
         name=name,
         email=email,
-        metadata=metadata
+        metadata=metadata,
     )
     if raw:
         return response
-    stripe_id = response.id
+    stripe_id = response.id 
     return stripe_id
 
 
-def create_product(name="",
-                   metadata={},
-                   raw=False):
+def create_product(name="", 
+        metadata={},
+        raw=False):
     response = stripe.Product.create(
         name=name,
         metadata=metadata,
     )
     if raw:
         return response
-    stripe_id = response.id
-    return stripe.id
-
+    stripe_id = response.id 
+    return stripe_id
 
 def create_price(currency="usd",
                 unit_amount="9999",
                 interval="month",
                 product=None,
                 metadata={},
-                raw=False):
+        raw=False):
     if product is None:
         return None
-    response =  stripe.Price.create(
-                currency=currency,
-                unit_amount=unit_amount,
-                recurring={"interval": interval},
-                product=product,
-                metadata=metadata
-            )
-    return stripe.id
+    response = stripe.Price.create(
+            currency=currency,
+            unit_amount=unit_amount,
+            recurring={"interval": interval},
+            product=product,
+            metadata=metadata
+        )
+    if raw:
+        return response
+    stripe_id = response.id 
+    return stripe_id
 
 
-def start_checkout_session(customer_id,
-                           success_url="", 
-                           cancel_url="", 
-                           price_stripe_id="", 
-                           raw=True):
+def start_checkout_session(customer_id, 
+        success_url="", 
+        cancel_url="", 
+        price_stripe_id="", 
+        raw=True):
     if not success_url.endswith("?session_id={CHECKOUT_SESSION_ID}"):
-        success_url = f"{success_url}" + "?session_id={{CHECKOUT_SESSION_ID}}" 
-    response = stripe.checkout.Session.create(
+        success_url = f"{success_url}" + "?session_id={CHECKOUT_SESSION_ID}"
+    response= stripe.checkout.Session.create(
         customer=customer_id,
         success_url=success_url,
         cancel_url=cancel_url,
@@ -89,22 +90,51 @@ def start_checkout_session(customer_id,
     )
     if raw:
         return response
-    else:
-        return response.url
-    
+    return response.url
+
 def get_checkout_session(stripe_id, raw=True):
-    response = stripe.checkout.Session.retrieve(
-        stripe_id
-    )
+    response =  stripe.checkout.Session.retrieve(
+            stripe_id
+        )
     if raw:
         return response
     return response.url
-    
-    
+
 def get_subscription(stripe_id, raw=True):
-    response = stripe.Subscription.retrieve(
-        stripe_id
-    )
+    response =  stripe.Subscription.retrieve(
+            stripe_id
+        )
+    if raw:
+        return response
+    return serialize_subscription_data(response)
+
+
+def get_customer_active_subscriptions(customer_stripe_id):
+    response =  stripe.Subscription.list(
+            customer=customer_stripe_id,
+            status="active"
+        )
+    return response
+
+
+def cancel_subscription(stripe_id, reason="", feedback="other", cancel_at_period_end=False, raw=True):
+    if cancel_at_period_end:
+        response =  stripe.Subscription.modify(
+                stripe_id,
+                cancel_at_period_end=cancel_at_period_end,
+                cancellation_details={
+                    "comment": reason,
+                    "feedback": feedback
+                }
+            )
+    else:
+        response =  stripe.Subscription.cancel(
+                stripe_id,
+                cancellation_details={
+                    "comment": reason,
+                    "feedback": feedback
+                }
+            )
     if raw:
         return response
     return serialize_subscription_data(response)
@@ -118,43 +148,11 @@ def get_checkout_customer_plan(session_id):
     # current_period_start
     # current_period_end
     sub_plan = sub_r.plan
-    
     subscription_data = serialize_subscription_data(sub_r)
     data = {
         "customer_id": customer_id,
         "plan_id": sub_plan.id,
         "sub_stripe_id": sub_stripe_id,
-        **subscription_data,
+       **subscription_data,
     }
     return data
-
-def get_customer_active_subscriptions(customer_stripe_id):
-    response = stripe.Subscription.list(
-        customer=customer_stripe_id,
-        status="active"
-    )
-    return response
-
-
-
-def cancel_subscription(sub_stripe_id, reason="", feedback="other", cancel_at_period_end=False, raw=True):
-    if cancel_at_period_end:
-        response = stripe.Subscription.modify(
-            sub_stripe_id,
-            cancel_at_period_end = cancel_at_period_end,
-            cancellation_details={
-                "comment": reason,
-                "feedback": feedback
-            }
-        )
-    else:
-        response = stripe.Subscription.cancel(
-            sub_stripe_id,
-            cancellation_details={
-                "comment": reason,
-                "feedback": feedback
-            }
-        )
-    if raw:
-        return response
-    return serialize_subscription_data(response)
--- src/helpers/downloader.py
+++ src/helpers/downloader.py
@@ -3,15 +3,15 @@ from pathlib import Path
 
 def download_to_local(url:str, out_path:Path, parent_mkdir:bool=True):
     if not isinstance(out_path, Path):
-        raise ValueError(f"{out_path} must be a valid pathlib Path object")
+        raise ValueError(f"{out_path} must be a valid pathlib.Path object")
     if parent_mkdir:
         out_path.parent.mkdir(parents=True, exist_ok=True)
     try:
         response = requests.get(url)
         response.raise_for_status()
-        # write the file out in binary mode to prevent newline conversions
+        # Write the file out in binary mode to prevent any newline conversions
         out_path.write_bytes(response.content)
         return True
-    except response.RequestException as e:
-        print(f"Fail to download {url}: {e}")
+    except requests.RequestException as e:
+        print(f'Failed to download {url}: {e}')
         return False
