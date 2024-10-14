a435b56302949683ae018e68d065da1875003205
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-14 10:06:59 +0800

    correction for cfehome and auth

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
@@ -8,10 +8,10 @@ User = get_user_model()
 
 # Create your views here.
 def login_view(request):
-    print(request.method, request.POST or None)
     if request.method == "POST":
         username = request.POST.get("username") or None
         password = request.POST.get("password") or None
+        # eval("print('hello')")
         if all([username, password]):
             user = authenticate(request, username=username, password=password)
             if user is not None:
@@ -20,16 +20,17 @@ def login_view(request):
                 return redirect("/")
     return render(request, "auth/login.html", {})
 
+
 def register_view(request):
     if request.method == "POST":
-        print(request.POST)
+        # print(request.POST)
         username = request.POST.get("username") or None
         email = request.POST.get("email") or None
-        password = request.POST.get("password") or None 
-        # Django Form
-        # user_exist_qs = User.objects.filter(username__iexact=username).exists()
-        # email_exist_qs = User.objects.filter(email__iexact=email).exists()
-        try: 
+        password = request.POST.get("password") or None
+        # Django Forms
+        # username_exists = User.objects.filter(username__iexact=username).exists()
+        # email_exists = User.objects.filter(email__iexact=email).exists()
+        try:
             User.objects.create_user(username, email=email, password=password)
         except:
             pass
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -14,33 +14,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
 from checkouts import views as checkout_views
-from subscriptions import views as subscription_views
 from landing import views as landing_views
-from .views import home_view, about_view, pw_protected_view, user_only_view, staff_only_view
+from subscriptions import views as subscriptions_views
+from .views import (
+    home_view, 
+    about_view, 
+    pw_protected_view,
+    user_only_view,
+    staff_only_view
+)
 
 urlpatterns = [
-    path("", landing_views.landing_dashboard_page_view, name='home'), #index page -> root page
-    # path("login/", auth_views.login_view),
-    # path("register/", auth_views.register_view),
-    path("checkout/sub-price/<int:price_id>/", checkout_views.product_price_redirect_view, name='sub-price-checkout'),
-    path("checkout/start/", checkout_views.checkout_redirect_view, name='stripe-checkout-start'),
-    path("checkout/success/", checkout_views.checkout_finalize_view, name='stripe-checkout-end'),
-    path("pricing/", subscription_views.subscription_price_view, name='pricing'),
-    path("pricing/<str:interval>/", subscription_views.subscription_price_view, name='pricing_interval'),
+    path("", landing_views.landing_dashboard_page_view, name='home'),
+    path("checkout/sub-price/<int:price_id>/", 
+            checkout_views.product_price_redirect_view,
+            name='sub-price-checkout'
+            ),
+    path("checkout/start/", 
+            checkout_views.checkout_redirect_view,
+            name='stripe-checkout-start'
+            ),
+    path("checkout/success/", 
+            checkout_views.checkout_finalize_view,
+            name='stripe-checkout-end'
+            ),
+    path("pricing/", subscriptions_views.subscription_price_view, name='pricing'),
+    path("pricing/<str:interval>/", subscriptions_views.subscription_price_view, name='pricing_interval'),
     path("about/", about_view),
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
-    path('accounts/billing/', subscription_views.user_subscription_view, name='user_subscription'),
-    path('accounts/billing/cancel/', subscription_views.user_subscription_cancel_view, name='user_subscription_cancel'),
+    path('accounts/billing/', subscriptions_views.user_subscription_view, name='user_subscription'),
+    path('accounts/billing/cancel', subscriptions_views.user_subscription_cancel_view, name='user_subscription_cancel'),
     path('accounts/', include('allauth.urls')),
-    path('protected/', pw_protected_view),
     path('protected/user-only/', user_only_view),
     path('protected/staff-only/', staff_only_view),
+    path('protected/', pw_protected_view),
     path('profiles/', include('profiles.urls')),
     path("admin/", admin.site.urls),
 ]
