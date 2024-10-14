5fc42cea69df362e4121e5cf43156848f338c1df
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-14 10:17:42 +0800

    update logics for profiles

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile_list_view),
-    path("<username>/", views.profile_detail_view),
+    path("", views.profile_list_view),
+    path("<str:username>/", views.profile_detail_view),
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -3,28 +3,34 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
 @login_required
-def profile_list_view(request, *args, **kwargs):
+def profile_list_view(request):
     context = {
         "object_list": User.objects.filter(is_active=True)
     }
     return render(request, "profiles/list.html", context)
 
 
-# Create your views here.
 @login_required
 def profile_detail_view(request, username=None, *args, **kwargs):
     user = request.user
-    user_group = user.groups.all()
+    print(
+        user.has_perm("subscriptions.basic"),
+        user.has_perm("subscriptions.basic_ai"),
+        user.has_perm("subscriptions.pro"),
+        user.has_perm("subscriptions.advanced"),    
+    )
+    # user_groups = user.groups.all()
+    # print("user_groups", user_groups)
+    # if user_groups.filter(name__icontains='basic').exists():
+    #     return HttpResponse("Congrats")
     profile_user_obj = get_object_or_404(User, username=username)
-    is_me = profile_user_obj == user  
+    is_me = profile_user_obj == user
     context = {
         "object": profile_user_obj,
         "instance": profile_user_obj,
         "owner": is_me,
     }
-    # return HttpResponse(f"Hello there! {username} - {profile_user_obj.id} - {user.id} - {is_me}") #f string
     return render(request, "profiles/detail.html", context)
